4b2e6c9e321c2eee0a3edb28cbc0e71f89bb9511
Andres <Andres@DESKTOP-S0CU8I1>
2017-03-27 19:29:20 +0200

    Test

---
+++ src/test/java/services/MessageServiceTest.java
@@ -0,0 +1,20 @@
+package services;
+
+import javax.transaction.Transactional;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:spring/junit.xml"})
+@Transactional
+public class MessageServiceTest extends AbstractTest{
+	//The SUT------------------------------------------------------
+	@Autowired
+	MessageService messageService;
+
+}
